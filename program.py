916fdf47d2737a957819d16d641396a11bc268b3
Gabriel Bożek <gabriel.jakub.bozek@gmail.com>
2020-05-28 22:56:58 +0200

    naprawienie bugow

--- program.py
+++ program.py
@@ -1,42 +1,35 @@
 import random
 from math import gcd
 
+
 def lenghtOfLongestAP(set):
     """
-    Zwraca długość najdłuższego podciągu arytmetycznego
-    :param set: lista liczb
-    :return: długość ciągu
-    """
+       Zwraca długość najdłuższego podciągu arytmetycznego
+       :param set: lista liczb
+       :return: długość ciągu
+       """
+    ans = 2
     n = len(set)
-    if (n <= 2):
+    if n <= 2:
         return n
-    L = [[0 for x in range(n)]
-         for y in range(n)]
-    llap = 2
-    for i in range(n):
-        L[i][n - 1] = 2
-    for j in range(n - 2, 0, -1):
+    llap = [2] * n
+    set.sort()
+
+    for j in range(n - 2, -1, -1):
         i = j - 1
         k = j + 1
-        while (i >= 0 and k <= n - 1):
-
-            if (set[i] + set[k] < 2 * set[j]):
-                k += 1
-
-            elif (set[i] + set[k] > 2 * set[j]):
-                L[i][j] = 2
+        while (i >= 0 and k < n):
+            if set[i] + set[k] == 2 * set[j]:
+                llap[j] = max(llap[k] + 1, llap[j])
+                ans = max(ans, llap[j])
                 i -= 1
-
+                k += 1
+            elif set[i] + set[k] < 2 * set[j]:
+                k += 1
             else:
-                L[i][j] = L[j][k] + 1
-                llap = max(llap, L[i][j])
                 i -= 1
-                k += 1
-                while (i >= 0):
-                    L[i][j] = 2
-                    i -= 1
-    return llap
 
+    return ans
 
 class pair_number_color:
     """
@@ -142,7 +135,7 @@ def print_colors(list_):
     """
     for c in list_:
         if c is None:
-            print("", end="\t")
+            print(" ", end="\t")
         else:
             print(c, end="\t")
 
@@ -168,7 +161,6 @@ def input_range(maxlenght):
 
 
 #-----------------------Skrypt gry--------------------------
-
 
 strategy_list = ["1. Losowy ciąg", "2. Kolejne liczy", "3. Ciąg EKG", "4. Połowy przedziałów"]
 
